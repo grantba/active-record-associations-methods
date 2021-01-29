4d698d18e9af84722ebe3b85de9d5ace5489a673
Blaire <grant.blaire@gmail.com>
2021-01-28 22:50:32 -0600

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,13 +4,28 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
+    self.songs.first.genre
   end
 
   def song_count
     #return the number of songs associated with the artist
+    self.songs.size
   end
 
   def genre_count
     #return the number of genres associated with the artist
+    self.genres.uniq.count
   end
 end
+
+# create_table "artists", force: :cascade do |t|
+#   t.string "name"
+# end
+# create_table "genres", force: :cascade do |t|
+#   t.string "name"
+# end
+# create_table "songs", force: :cascade do |t|
+#   t.string "name"
+#   t.integer "artist_id"
+#   t.integer "genre_id"
+# end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,13 +4,28 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
+    self.songs.size
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    self.artists.size
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    self.artists.collect {|artist| artist.name }
   end
 end
+
+# create_table "artists", force: :cascade do |t|
+#   t.string "name"
+# end
+# create_table "genres", force: :cascade do |t|
+#   t.string "name"
+# end
+# create_table "songs", force: :cascade do |t|
+#   t.string "name"
+#   t.integer "artist_id"
+#   t.integer "genre_id"
+# end
